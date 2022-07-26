1ea4bdc08c8bdf24c03c3080e078570561f9027e
Y4er <45418382@qq.com>
2022-07-26 09:35:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 yum install java-1.8.0-openjdk* wget -y -q
-wget -q https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz
+wget -q https://archive.apache.org/dist/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz
 tar -xvf apache-maven-3.8.5-bin.tar.gz
 ./apache-maven-3.8.5/bin/mvn -DskipTests=true package
